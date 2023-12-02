f0af9840d3117da6c5e4450e219a51636f7b7e9d
Mina Girgis <89814001+Mina-Girgis@users.noreply.github.com>
2023-12-02 09:43:34 +0200

    done

--- build/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.app = void 0;
-const express = require("express");
-exports.app = express();
-const userRoute = require("./routes/userRoutes");
-exports.app.use(express.json());
-exports.app.use("/", userRoute);
